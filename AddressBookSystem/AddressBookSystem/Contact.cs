7fe6b5a353ece2249a5b18cdb04a98be3c15352d
Soumya <soumyadipta0077@gmail.com>
2020-10-03 13:29:30 +0530

    [Soumya] Add: Adding Contact

---
+++ AddressBookSystem/AddressBookSystem/Contact.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookSystem
+{
+    class Contact
+    {
+        public string fistName { get; set; }
+        public string lastName { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public int zip { get; set; }
+        public long phoneNumber { get; set; }
+        public string email { get; set; }
+    }
+}
---
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AddressBookSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddressBook addressBook = new AddressBook();
+            addressBook.addContacts("Soumya", "Banerjee", "Ranibagan", "Berhampore", "West Bengal", 742101, 9898989898, "abcd@gmail.com");
+            addressBook.print();
+        }
+    }
+}
